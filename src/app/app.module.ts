7873ddf4b18ebde8c2368d909934fbc8308d5284
Alexander Friesen <friesen.alex.home@gmail.com>
2021-05-14 12:57:02 +0200

    Update to angular 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,12 @@ import { environment } from '../environments/environment';
     ThemeModule,
     TemplateModule,
     ContainerModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   declarations: [
     AppComponent
