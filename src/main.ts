4e9444e2eb9afc3eb3d79e3adb6315d03ddc1ffc
Alexander Friesen <friesen.alex.home@gmail.com>
2022-12-06 12:17:53 +0100

    enable animations

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserAnimationsModule)],
+}).catch((err) => console.error(err));
